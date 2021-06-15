73d30d8a7208907467ceb9b247e67f7299e80228
hellishBlemish <76493431+hellishBlemish@users.noreply.github.com>
2021-06-15 08:37:54 -0400

    Will Github replace?
    Two things are being tested right now. First of all, will github replace the files I already have with this? Start a new branch? Just outright tell me I cannot upload files of the same name to the same repository/branch? Change the name of one??? Who knows, not me.
    Second of all, will the squirrel run? Spent a while looking for the easiest way to make the little nut-eater run, and it was not worth it because I just ended up using frames again. Bluh. Whatever, tis what tis.

--- shinyday.js
+++ shinyday.js
@@ -4,6 +4,8 @@ var fantaRingtone = new Audio('https://freesound.org/data/previews/276/276608_44
 var winningSFX = new Audio('https://freesound.org/data/previews/269/269198_4409114-lq.mp3');
 var clip = document.getElementById('clip');
 var fight = document.getElementById('fighters');
+var game = document.getElementById('gameart');
+var sq = document.getElementById('squirrel');
 
 function playClip(){
   clip.classList.add('play');
@@ -19,4 +21,69 @@ function playFight(){
   setTimeout(function(){fight.classList.remove('play')},2500);
   document.querySelector('#fighters button').classList.remove('new');
   return console.log('Playing: Fight');
+}
+
+window.addEventListener('resize', function(){
+  game.style.transform = 'scale('+ checkScale() +')';
+  game.style.left = '-'+544/2*(1-checkScale())+'px';
+  game.style.top = '-'+416/2*(1-checkScale())+'px';
+});
+
+function checkScale(){
+  if(window.innerWidth/544 < 1){
+    return window.innerWidth/544;
+  }else{
+    return 1;
+  }
+}
+
+window.onload = function(){
+  game.style.transform = 'scale('+ checkScale() +')';
+  game.style.left = '-'+544/2*(1-checkScale())+'px';
+  game.style.top = '-'+416/2*(1-checkScale())+'px';
+  window.setInterval(function(){
+    if(!sq.classList.contains('run')){
+      sq.classList.replace('idle','looking');
+      setTimeout(function(){
+        sq.classList.replace('looking','idle');
+      },1000*getRandomInt(1,3));
+    };
+  }, getRandomInt(5000,20000));
+}
+
+function pokeSquirrel(){
+  if(sq.classList.contains('run')){
+    return console.log('Already running!');
+  }
+  if(sq.classList.contains('looking')){
+    sq.classList.replace('looking','idle');
+  }
+  if(sq.classList.contains('first-base')){
+    sq.classList.replace('first-base','second-base');
+    sq.classList.replace('idle','run');
+    setTimeout(function(){
+      sq.classList.add('left');
+      sq.classList.replace('run','idle');
+    },3999);
+  }else if(sq.classList.contains('second-base')){
+    sq.classList.replace('second-base','first-base');
+    sq.classList.replace('idle','run');
+    setTimeout(function(){
+      sq.classList.remove('left');
+      sq.classList.replace('run','idle');
+    },3999);
+  }else{
+    sq.classList.add('second-base');
+    sq.classList.replace('idle','run');
+    setTimeout(function(){
+      sq.classList.add('left');
+      sq.classList.replace('run','idle');
+    },3999);
+  }
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
